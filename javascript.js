0d15a57df5f3f18713ab96f681b8480474991ace
talaemad <talae.ishream@gmail.com>
2020-12-27 14:55:07 +0200

    added js and css

--- javascript.js
+++ javascript.js
@@ -1,6 +1,4 @@
-alert('Welcome!');
-var name= prompt('please enter your name');
-
+/*
 var gender = prompt('please enter (f) if you are fmale and (m) if you are male');
 if (gender=='f'){
     document.write('<p> <b> Madam '+name+'</b> </p>');
@@ -9,15 +7,28 @@ else if (gender=='m'){
     document.write('<p> <b> Sir '+name+'</b> </p>');
 }
 document.write('<p> We are happy that you chose us and we hope that you have a good day with a good experience.</p>' )
+*/
 
 // the new addedd 24Dec
-var number= prompt('how many time you would like to see the view of our resturent');
 
-while(number == null || number == ''){
- number= prompt('how many time you would like to see the view of our resturent');
+function alrt(msg) {
+ alert(msg);
 }
+function validation(msg1) {
+    var value1 = prompt(msg1);
+    while(value1 === undefined || value1 === null || value1 === ''){
+       value1 = prompt(msg1);
+    }
+    return value1;
+}
+
+alrt("Welcome to our page");
+var uname=validation('please enter your name');
+alrt("Hello "+uname)
 var addpic=''
-for (i=1; i <= number;i++){
-addpic=addpic+'<p>'+i+'<img src="download.jpeg"width="200" height="200">'+'</p>';
+val=validation('how many time you would like to see the view of our resturent');
+
+for (i=1; i <= val;i++){
+ addpic=addpic+'<p>'+i+'<img src="download.jpeg"width="200" height="200">'+'</p>';
 }
 document.write(addpic);
